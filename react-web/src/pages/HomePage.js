82b5583454701f014c7ed8630e6b1494052e0ceb
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2022-09-28 13:00:55 -0700

    Initial

---
+++ react-web/src/pages/HomePage.js
@@ -0,0 +1,196 @@
+import { Box, Button, ChakraProvider, Divider, Heading, Highlight, Image, Text } from "@chakra-ui/react";
+import Header from "../comps/Header";
+import theme from "../theme";
+
+import Wave1 from  '../assets/wave-2.png'
+
+function HomePage() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box>
+        <Header />
+        <Box>
+
+          <Box h={'70%'} w={'100vw'} p={'50'}>
+
+            <Heading 
+              textAlign={'center'}
+              bgGradient='radial(#E20D8B, #6459F4,)'
+              bgClip='text'
+              size={'4xl'}
+              pr={'15'}
+              pl={'15'}
+              w={'50vw'}
+              margin={'auto'}
+              lineHeight={'1.2'}
+              fontWeight={'extrabold'}
+            >
+              The easiest way to track all your digital analytics.
+            </Heading>
+
+            <Text
+              textAlign={'center'}
+              fontSize={'xl'}
+              fontWeight={'bold'}
+              color={'gray.500'}
+              w={'50vw'}
+              margin={'auto'}
+              mt={10}
+            >
+              Mirky allows you to get privacy first analytics on any website or app.
+            </Text>
+
+            <Box
+              display={'flex'}
+              justifyContent={'center'}
+              alignItems={'center'}
+              mt={10}
+              mb={10}
+              flexDir={{ base: 'column', md: 'row' }}
+            >
+              <Button
+                mt={50}
+                colorScheme={"brandPurple"}
+                size={'lg'}
+                fontWeight={'bold'}
+                fontSize={'3xl'}
+                p={8}
+                mr={5}
+                ml={5}
+              >
+                Get Started
+              </Button>
+
+              <Button
+                mt={50}
+                colorScheme={"brandPurple"}
+                size={'lg'}
+                variant='outline'
+                fontWeight={'bold'}
+                fontSize={'3xl'}
+                ml={5}
+                mr={5}
+                p={8}
+              >
+                Learn More
+              </Button>
+
+              
+            </Box>
+          </Box>
+
+          <Image src={Wave1} />
+
+          <Box
+            textAlign={'center'}
+            w={'50vw'}
+            margin={'auto'}
+          >
+            
+            <Box 
+              alignContent={'center'} 
+              alignItems={'center'}
+              justifyContent={'center'} 
+              display={'flex'} 
+            >
+              <Heading size={'2xl'}>Why</Heading>
+              <Box w={3}></Box>
+              <Heading
+                bgGradient='linear(to-r, brandPink.500, brandBlurple.500)' 
+                bgClip='text'
+                size={'2xl'}
+                fontWeight={'extrabold'}
+              >
+                Mirky
+              </Heading>
+              <Box w={1}></Box>
+              <Heading size={'2xl'}>?</Heading>
+            </Box>
+
+            <Text
+              pt={5}
+              color={'gray.400'}
+              fontWeight={'bold'}
+              lineHeight={'2'}
+            >
+              Mirky is a privacy first analytics platform that allows you to track all your digital analytics in one place. We provide code snippets, APIs, and packages for any platform, language, or framework. No cookies and fully compliant with GDPR, CCPA and PECR. With our state of the art public dashboards, you can share non-sensitive analytics with your visitors. We offer integrations with a plethora of apps, including Notion, Slack, Discord, and more. Create data alerts via webhooks, to be notified when specified events occur. Don't get trapped in vendor lock-in, ditch those big guys and come to Mirky.
+            </Text>
+
+          </Box>
+
+          <Box h={10}></Box>
+
+          <Box 
+            alignContent={'center'} 
+            alignItems={'center'}
+            justifyContent={'center'} 
+            display={'flex'} 
+            flex={2}
+            flexDir={{ base: 'column', md: 'row' }} 
+            pl={50} pr={50} pb={50}
+            mt={10}
+            >
+
+            <Box 
+              w={'20rem'} 
+              ml={5} mr={5} mb={5} 
+              h={'15rem'} 
+              backgroundColor={'whiteAlpha.300'} 
+              borderRadius={20}
+              p={8}
+              textAlign={'center'}
+              boxShadow={'base'}
+            >
+              <Heading pb={2} bgGradient='linear(to-r, brandPink.500, brandBlurple.500)' bgClip='text'>Private</Heading>
+              <Divider />
+
+              <Text pt={5}>
+                Privacy is in our DNA. All our data is encrypted with per-user unique keys, meaning only you can decrypt your data.
+              </Text>
+            </Box>
+
+            <Box 
+              w={'20rem'} 
+              ml={5} mr={5} mb={5} 
+              h={'15rem'} 
+              backgroundColor={'whiteAlpha.300'} 
+              borderRadius={20}
+              p={8}
+              textAlign={'center'}
+              boxShadow={'base'}
+            >
+              <Heading pb={2} bgGradient='linear(to-r, brandBlurple.500, brandPurple.500)' bgClip='text'>Anywhere</Heading>
+              <Divider />
+
+              <Text pt={5}>
+                We provide tools to use Mirky on any website or app, no matter what framework or language you use.
+              </Text>
+            </Box>
+
+            <Box 
+              w={'20rem'} 
+              ml={5} mr={5} mb={5} 
+              h={'15rem'} 
+              backgroundColor={'whiteAlpha.300'} 
+              borderRadius={20}
+              p={8}
+              textAlign={'center'}
+              boxShadow={'base'}
+            >
+              <Heading pb={2} bgGradient='linear(to-r, brandPurple.500, brandBlue.500)' bgClip='text'>Easy</Heading>
+              <Divider />
+
+              <Text pt={5}>
+                Our systems are easy to impliment, with as little as one line of code. Data starts being collected immediately.
+              </Text>
+            </Box>
+
+          </Box>
+
+        </Box>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default HomePage;
---
+++ react-web/src/theme.js
@@ -0,0 +1,66 @@
+import { extendTheme } from '@chakra-ui/react'
+
+// 2. Add your color mode config
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: true,
+  styles: {
+    global: {
+      body: {
+        bg: 'black',
+        color: 'white',
+      },
+    },
+  },
+  colors: {
+    brandPink: {
+      100: "#F086C5",
+      200: "#ED68B7",
+      300: "#E94AA8",
+      400: "#E62B9A",
+      500: "#E20D8B",
+      600: "#C60B7A",
+      700: "#A90A68",
+      800: "#8D0857",
+      900: "#710746",
+    },
+    brandBlurple: {
+      100: "#B2ACFA",
+      200: "#9E97F8",
+      300: "#8B83F7",
+      400: "#776EF5",
+      500: "#6459F4",
+      600: "#584ED6",
+      700: "#4B43B7",
+      800: "#3F3899",
+      900: "#322C7A",
+    },
+    brandBlue: {
+      100: "#A4BBFD",
+      200: "#8DA9FC",
+      300: "#7798FC",
+      400: "#6087FB",
+      500: "#4976FB",
+      600: "#4067DC",
+      700: "#3759BC",
+      800: "#2E4A9D",
+      900: "#253B7D",
+    },
+    brandPurple: {
+      100: "#D593E1",
+      200: "#CA77DA",
+      300: "#C05CD2",
+      400: "#B541CB",
+      500: "#AB26C3",
+      600: "#9621AB",
+      700: "#801D92",
+      800: "#6B187A",
+      900: "#551362",
+    }
+  }
+}
+
+// 3. extend the theme
+const theme = extendTheme(config)
+
+export default theme
